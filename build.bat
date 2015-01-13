db5000760c9b6d4b0caa569099ca3eaeda46f11a
Nikos M <nikosms0@gmail.com>
2015-01-13 20:07:56 +0200

    update buildtools

--- build.bat
+++ build.bat
@@ -3,15 +3,15 @@
 REM ###################################################
 REM #
 REM #   The buildtools repository is at:
-REM #   https://github.com/foo123/scripts/buildtools
+REM #   https://github.com/foo123/Beeld
 REM #
 REM ###################################################
 
 REM to use the python build tool do:
-REM python ..\Beeld\Beeld.py --config ".\config.custom"
+REM python %BUILDTOOLS%\Beeld.py --config ".\config.custom"
 
 REM to use the php build tool do:
-REM php -f ..\Beeld\Beeld.php -- --config=".\config.custom"
+REM php -f %BUILDTOOLS%\Beeld.php --  --config=".\config.custom"
 
 REM to use the node build tool do:
-node ..\Beeld\Beeld.js --config ".\config.custom"
+node %BUILDTOOLS%\Beeld.js  --config ".\config.custom"
